40a1c1d37956ef18fe7daeaa7a36b30478e3b573
Sakshiinegi <negisakshi564@gmail.com>
2025-05-04 16:14:17 +0530

    Bisection method.cpp

---
+++ Bisection method.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+double f(double x) {
+    return x*x*x - x - 2;
+}
+
+int main() {
+    double a = 1, b = 2, c;
+    double error = 0.0001;
+
+    if (f(a) * f(b) >= 0) {
+        cout << "Invalid interval! f(a) and f(b) should have opposite signs." << endl;
+        return 1;
+    }
+
+    while ((b - a) >= error) {
+        c = (a + b) / 2;
+
+        if (f(c) == 0.0)
+            break;
+            
+        else if (f(c) * f(a) < 0)
+            b = c;
+        else
+            a = c;
+    }
+
+    cout << "The root is: " << c << endl;
+    return 0;
+}
